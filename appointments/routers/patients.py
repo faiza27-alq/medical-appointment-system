a82566a76d8dfabd7af868d98ae9d1c1d8afc952
faiza27-alq <faizaalqzyva@gmail.com>
2024-12-24 23:06:02 +0400

    Create patients.py

---
+++ appointments/routers/patients.py
@@ -0,0 +1,31 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app import crud, models, schemas
+from app.database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
+
+router = APIRouter()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@router.get("/", response_model=list[schemas.Patient])
+def read_patients(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    patients = crud.get_patients(db, skip=skip, limit=limit)
+    return patients
+
+@router.get("/{patient_id}", response_model=schemas.Patient)
+def read_patient(patient_id: int, db: Session = Depends(get_db)):
+    db_patient = crud.get_patient(db, patient_id=patient_id)
+    if db_patient is None:
+        raise HTTPException(status_code=404, detail="Patient not found")
+    return db_patient
+
+@router.post("/", response_model=schemas.Patient)
+def create_patient(patient: schemas.PatientCreate, db: Session = Depends(get_db)):
+    return crud.create_patient(db=db, patient=patient)
