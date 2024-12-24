28b77d95da615241c7b7715e480e096cfff21d67
faiza27-alq <faizaalqzyva@gmail.com>
2024-12-24 20:43:19 +0400

    Create models.py

---
+++ appointments/models.py
@@ -0,0 +1,25 @@
+# models.py
+from django.db import models
+
+class Doctor(models.Model):
+    name = models.CharField(max_length=100)
+    specialty = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Patient(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Appointment(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+
+    def __str__(self):
+        return f"{self.doctor} - {self.patient} ({self.date} {self.time})"
