5443d8fcb842675debf4983fb17cf15fcf5ed0ea
faiza27-alq <faizaalqzyva@gmail.com>
2024-12-24 23:02:30 +0400

    Create schemas.py

---
+++ appointments/schemas.py
@@ -0,0 +1,46 @@
+from pydantic import BaseModel
+from datetime import date, time
+
+class DoctorBase(BaseModel):
+    name: str
+    specialty: str
+
+class DoctorCreate(DoctorBase):
+    pass
+
+class Doctor(DoctorBase):
+    id: int
+    appointments: list
+
+    class Config:
+        orm_mode = True
+
+class PatientBase(BaseModel):
+    name: str
+    age: int
+
+class PatientCreate(PatientBase):
+    pass
+
+class Patient(PatientBase):
+    id: int
+    appointments: list
+
+    class Config:
+        orm_mode = True
+
+class AppointmentBase(BaseModel):
+    date: date
+    time: time
+
+class AppointmentCreate(AppointmentBase):
+    doctor_id: int
+    patient_id: int
+
+class Appointment(AppointmentBase):
+    id: int
+    doctor: Doctor
+    patient: Patient
+
+    class Config:
+        orm_mode = True
