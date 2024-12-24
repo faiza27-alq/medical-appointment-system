12277548c60ca933e2d0eb07f230853a1a601a1a
faiza27-alq <faizaalqzyva@gmail.com>
2024-12-24 23:02:59 +0400

    Create crud.py

---
+++ appointments/crud.py
@@ -0,0 +1,45 @@
+from sqlalchemy.orm import Session
+from app import models, schemas
+
+def get_doctors(db: Session, skip: int = 0, limit: int = 10):
+    return db.query(models.Doctor).offset(skip).limit(limit).all()
+
+def get_doctor(db: Session, doctor_id: int):
+    return db.query(models.Doctor).filter(models.Doctor.id == doctor_id).first()
+
+def create_doctor(db: Session, doctor: schemas.DoctorCreate):
+    db_doctor = models.Doctor(name=doctor.name, specialty=doctor.specialty)
+    db.add(db_doctor)
+    db.commit()
+    db.refresh(db_doctor)
+    return db_doctor
+
+def get_patients(db: Session, skip: int = 0, limit: int = 10):
+    return db.query(models.Patient).offset(skip).limit(limit).all()
+
+def get_patient(db: Session, patient_id: int):
+    return db.query(models.Patient).filter(models.Patient.id == patient_id).first()
+
+def create_patient(db: Session, patient: schemas.PatientCreate):
+    db_patient = models.Patient(name=patient.name, age=patient.age)
+    db.add(db_patient)
+    db.commit()
+    db.refresh(db_patient)
+    return db_patient
+
+def get_appointments(db: Session, skip: int = 0, limit: int = 10):
+    return db.query(models.Appointment).offset(skip).limit(limit).all()
+
+def get_appointment(db: Session, appointment_id: int):
+    return db.query(models.Appointment).filter(models.Appointment.id == appointment_id).first()
+
+def create_appointment(db: Session, appointment: schemas.AppointmentCreate):
+    db_appointment = models.Appointment(date=appointment.date, time=appointment.time, doctor_id=appointment.doctor_id, patient_id=appointment.patient_id)
+    db.add(db_appointment)
+    db.commit()
+    db.refresh(db_appointment)
+    return db_appointment
+
+def delete_appointment(db: Session, appointment_id: int):
+    db.query(models.Appointment).filter(models.Appointment.id == appointment_id).delete()
+    db.commit()
