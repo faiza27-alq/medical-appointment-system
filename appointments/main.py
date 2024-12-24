534f0440a6f56c2e1db08f9105341336eb269e74
faiza27-alq <faizaalqzyva@gmail.com>
2024-12-24 23:01:19 +0400

    Create main.py

---
+++ appointments/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from app.routers import doctors, patients, appointments
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Medical Appointment System"}
+
+app.include_router(doctors.router, prefix="/doctors", tags=["Doctors"])
+app.include_router(patients.router, prefix="/patients", tags=["Patients"])
+app.include_router(appointments.router, prefix="/appointments", tags=["Appointments"])
