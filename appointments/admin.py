1df2a872def9f7001bf3fa399a8f1f1fdd4adafa
faiza27-alq <faizaalqzyva@gmail.com>
2024-12-24 20:41:59 +0400

    Create admin.py

---
+++ appointments/admin.py
@@ -0,0 +1,7 @@
+# admin.py
+from django.contrib import admin
+from .models import Doctor, Patient, Appointment
+
+admin.site.register(Doctor)
+admin.site.register(Patient)
+admin.site.register(Appointment)
