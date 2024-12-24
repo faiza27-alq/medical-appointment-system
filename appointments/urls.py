e6a2adb8d59c4f02919e190b32a294abc620bb5d
faiza27-alq <faizaalqzyva@gmail.com>
2024-12-24 21:12:25 +0400

    Update urls.py

--- appointments/urls.py
+++ appointments/urls.py
@@ -1,11 +1,12 @@
-# urls.py
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
 
+router = DefaultRouter()
+router.register(r'doctors', views.DoctorViewSet)
+router.register(r'patients', views.PatientViewSet)
+router.register(r'appointments', views.AppointmentViewSet)
+
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('doctors/', views.doctor_list, name='doctor_list'),
-    path('patients/', views.patient_list, name='patient_list'),
-    path('appointments/', views.appointment_list, name='appointment_list'),
-    path('appointments/create/', views.appointment_create, name='appointment_create'),
+    path('', include(router.urls)),
 ]
