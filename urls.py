c8598f604d017ffdabb9bd5e9eac8ec0954fb176
faiza27-alq <faizaalqzyva@gmail.com>
2024-12-24 21:15:25 +0400

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,8 +1,20 @@
-# urls.py
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Medical Appointment API",
+      default_version='v1',
+      description="API documentation for Medical Appointment System",
+   ),
+   public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('appointments/', include('appointments.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
