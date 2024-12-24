d433cb91f7c5e48c25e4f39cea9584fecf177d97
faiza27-alq <faizaalqzyva@gmail.com>
2024-12-24 20:39:17 +0400

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,14 @@
+# settings.py
+INSTALLED_APPS = [
+    ...
+    'appointments',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
