116c88bcbe5ef5ff6229c1e67739531c322b43b8
faiza27-alq <faizaalqzyva@gmail.com>
2024-12-25 12:37:58 +0400

    Update main.py

--- appointments/main.py
+++ appointments/main.py
@@ -1,17 +1,12 @@
 import os
-import django
 from fastapi import FastAPI
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medical_appointment_system.settings')
-django.setup()
+from appointments.routers import doctors, patients, appointments
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the Medical Appointment System"}
-
-from appointments.routers import doctors, patients, appointments
 
 app.include_router(doctors.router, prefix="/doctors", tags=["Doctors"])
 app.include_router(patients.router, prefix="/patients", tags=["Patients"])
